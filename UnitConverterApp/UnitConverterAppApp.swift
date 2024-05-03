f7db24a086b62a555ce2edd6eba245c4a8d25715
Pranav149 <pranavramesh84@gmail.com>
2024-05-03 00:00:58 -0700

    Initial Commit

---
+++ UnitConverterApp/UnitConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterAppApp.swift
+//  UnitConverterApp
+//
+//  Created by Pranav Ramesh on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
